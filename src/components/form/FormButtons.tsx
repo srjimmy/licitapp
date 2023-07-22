200f654528ab89c3217933bd35dc943aac24ec9c
srjimmy <72458798+srjimmy@users.noreply.github.com>
2023-07-22 12:15:22 +0200

    Finish implementation of Form component

--- src/components/form/FormButtons.tsx
+++ src/components/form/FormButtons.tsx
@@ -1,13 +1,38 @@
 import { Box, Button } from "@mui/material";
 
+import usePblStore from "@stores/pblStore";
+import useLicitadoresStore from "@stores/licitadoresStore";
+import useResultadosStore from "@stores/resultadosStore";
+
 export default function FormButtons() {
+  const pbl = usePblStore((state) => state.pbl);
+  const resetPbl = usePblStore((state) => state.resetPbl);
+
+  const licitadores = useLicitadoresStore((state) => state.licitadores);
+  const addLicitador = useLicitadoresStore((state) => state.addLicitador);
+  const rmLicitador = useLicitadoresStore((state) => state.rmLicitador);
+  const resetLicitador = useLicitadoresStore((state) => state.resetLicitador);
+
+  const setPbl = useResultadosStore((state) => state.setPbl);
+  const calcLicitacion = useResultadosStore((state) => state.calcLicitacion);
+
+  const resetForm = () => {
+    resetPbl();
+    resetLicitador();
+  };
+
+  const resultados = () => {
+    setPbl(pbl);
+    calcLicitacion(pbl, licitadores);
+  };
+
   return (
     <Box sx={{ display: "flex", alignItems: "center", gap: 3, mb: 3 }}>
       <Button
         color="success"
         variant="contained"
         sx={{ width: "100%" }}
-        // onClick={}
+        onClick={addLicitador}
       >
         Añadir
       </Button>
@@ -16,27 +41,25 @@ export default function FormButtons() {
         color="error"
         variant="contained"
         sx={{ width: "100%" }}
-        // onClick={}
+        onClick={rmLicitador}
       >
         Quitar
       </Button>
 
       <Button
-        type="reset"
         color="secondary"
         variant="contained"
         sx={{ width: "100%" }}
-        // onClick={}
+        onClick={resetForm}
       >
         Reset
       </Button>
 
       <Button
-        type="submit"
         color="primary"
         variant="contained"
         sx={{ width: "100%" }}
-        // onSubmit={}
+        onClick={resultados}
       >
         Calcular
       </Button>
--- src/components/form/FormFields.tsx
+++ src/components/form/FormFields.tsx
@@ -1,14 +1,30 @@
 import { Box, TextField } from "@mui/material";
 
 import usePblStore from "@stores/pblStore";
+import useLicitadoresStore from "@stores/licitadoresStore";
+
+type HIE = React.ChangeEvent<HTMLInputElement>;
 
 export default function FormFields() {
   const pbl = usePblStore((state) => state.pbl);
   const setPbl = usePblStore((state) => state.setPbl);
 
-  const handlePblChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePblChange = (event: HIE) => {
     const newValue = parseInt(event.target.value, 10);
     setPbl(newValue);
+  };
+
+  const licitadores = useLicitadoresStore((state) => state.licitadores);
+  const setLicitador = useLicitadoresStore((state) => state.setLicitador);
+
+  const handleNombreChange = (index: number, event: HIE) => {
+    const newNombre = event.target.value;
+    setLicitador(index, newNombre, licitadores[index].oferta);
+  };
+
+  const handleOfertaChange = (index: number, event: HIE) => {
+    const newOferta = parseInt(event.target.value, 10);
+    setLicitador(index, licitadores[index].nombre, newOferta);
   };
 
   return (
@@ -22,38 +38,40 @@ export default function FormFields() {
           size="small"
           variant="outlined"
           sx={{ width: "100%" }}
-          value={pbl}
+          value={isNaN(pbl) ? "" : pbl}
           onChange={handlePblChange}
         />
       </Box>
 
-      <Box
-        // key={}
-        sx={{ display: "flex", alignItems: "center", gap: 3, mb: 2 }}
-      >
-        <TextField
-          required
-          name="nombre"
-          label="Licitador"
-          type="text"
-          size="small"
-          variant="outlined"
-          sx={{ width: "60%" }}
-          // value={}
-          // onChange={}
-        />
-        <TextField
-          required
-          name="oferta"
-          label="Oferta"
-          type="number"
-          size="small"
-          variant="outlined"
-          sx={{ width: "40%" }}
-          // value={}
-          // onChange={}
-        />
-      </Box>
+      {licitadores.map((licitador, index) => (
+        <Box
+          key={index}
+          sx={{ display: "flex", alignItems: "center", gap: 3, mb: 2 }}
+        >
+          <TextField
+            required
+            name="nombre"
+            label="Licitador"
+            type="text"
+            size="small"
+            variant="outlined"
+            sx={{ width: "60%" }}
+            value={licitador.nombre}
+            onChange={(event: HIE) => handleNombreChange(index, event)}
+          />
+          <TextField
+            required
+            name="oferta"
+            label="Oferta"
+            type="number"
+            size="small"
+            variant="outlined"
+            sx={{ width: "40%" }}
+            value={isNaN(licitador.oferta) ? "" : licitador.oferta}
+            onChange={(event: HIE) => handleOfertaChange(index, event)}
+          />
+        </Box>
+      ))}
     </Box>
   );
 }
